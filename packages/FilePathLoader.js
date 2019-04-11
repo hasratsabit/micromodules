f80f9b37c426f0cd09f4b1d4a7fe75766da6a91d
Hasrat <hasrat.sabit@pwc.com>
2019-04-10 23:12:06 -0400

    Refined and pathing imporved

--- packages/FilePathLoader.js
+++ packages/FilePathLoader.js
@@ -3,30 +3,41 @@ const path = require('path');
 class FilePathLoader {
     constructor() {}
 
-    loadPaths(dirPath, identifier) {
+    formatFilePath (dirPath, file, identifier) {
+        let splitedFile = file.split(".");
+        let isMatchedIdentifier = (splitedFile.length && splitedFile.length > 2)  ? splitedFile[splitedFile.length - 2] === identifier : false;
+        if(!isMatchedIdentifier) return false;
+        splitedFile.splice(splitedFile.indexOf("js"));
+        let combinedDirs = dirPath.split("/");
+        // If the first directory is the same as the formatted dir passed to loadPaths, delete the passed root dir.
+        if(combinedDirs[0] === this.formattedDirPath) combinedDirs.shift();
+        combinedDirs = path.join(this.directoryPath, combinedDirs.toString()); // Add the main the dir with full path
+        let formatedFilePath = path.join(combinedDirs, splitedFile.join(".")); // Add combined dirs with formated file.
+        return formatedFilePath;
+    }
+
+    loadPaths(directoryPath, identifier) {
+        this.directoryPath = directoryPath; // Make it available to the entire class.
+        this.formattedDirPath = directoryPath.split(".").join("").split(""); // Delete any dot passed as full path and recreate char array.
+        if(this.formattedDirPath[0] === '/') this.formattedDirPath.shift();
+        this.formattedDirPath = this.formattedDirPath.join("");
+        
         let filesPathArray = [];
         let isDirectory = (source) => lstatSync(source).isDirectory();
-        const processFilePath = (dirPath, file, identifier) => {
-            let splitedFile = file.split(".");
-            let isMatchedIdentifier = (splitedFile.length && splitedFile.length > 2)  ? splitedFile[splitedFile.length - 2] === identifier : false;
-            if(!isMatchedIdentifier) return false;
-            // splitedFile.splice(splitedFile.indexOf("js"));
-            let formatedFilePath = path.join(dirPath, file);
-            return formatedFilePath;
-        }
-        if(!isDirectory(dirPath)) throw new Error("Please provide a directory path.");
-        readdirSync(dirPath).map(dirContent => {
-            let nestedDir = path.join(dirPath, dirContent);
+        if(!isDirectory(this.formattedDirPath)) throw new Error("Please provide a directory path.");
+        readdirSync(this.formattedDirPath).map(dirContent => {
+            let nestedDir = path.join(this.formattedDirPath, dirContent);
             if(!isDirectory(nestedDir)) {
-                let isValidPath = processFilePath(dirPath, dirContent, identifier);
+                let isValidPath = this.formatFilePath(directoryPath, dirContent, identifier);
                 if(isValidPath) filesPathArray.push(isValidPath);
             } 
             else return readdirSync(nestedDir).map(nestedFile => {
-               let validFilePath = processFilePath(nestedDir, nestedFile, identifier)
+               let validFilePath = this.formatFilePath(nestedDir, nestedFile, identifier)
                if(validFilePath) filesPathArray.push(validFilePath);
             })
         });
         return filesPathArray;
+       
     }
 }
 
